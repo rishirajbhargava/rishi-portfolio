fe9ece8344b13282b4471c940857bfbceccbfadb
rishirajbhargava <rishiraj.bhargava00@gmail.com>
2023-03-30 01:16:01 +0530

    not found page added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React , {useState, useEffect} from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
@@ -8,18 +9,23 @@ import Contact from './components/Contact';
 import Footer from './components/Footer';
 
 import { ScaleLoader } from "react-spinners";
+import NotFound from "./components/NotFound";
 
 
 
 
 function App() {
+
   const [loading , setLoading]= useState(false);
+
   useEffect(()=>{
     setLoading(true)
     setTimeout(()=>{
       setLoading(false)
     },1500)
   },[])
+
+
 
   const override = {
     "display": "flex",
@@ -30,32 +36,39 @@ function App() {
 
 
   return (
-    <div className="App">
-        {
-        loading ? <div className="loader">
-        <ScaleLoader
-        color={"#000"}
-        loading={loading}
-        cssOverride={override}
-        height={15}
-        width={3}
-        aria-label="Loading Spinner"
-        data-testid="loader"
-      />
-        </div>
-        
-        :<>
-      <Header/>
-      <Home/>
-       <About/>
-       <Projects/>
-       <Contact/>
-       <Footer/>
-      </>
-      }
-       
-       
-    </div>
+    <Router>
+       <Routes>
+       <Route path='/' element={
+          <div className="App">
+            {
+            loading ? <div className="loader">
+                <ScaleLoader
+                color={"#000"}
+                loading={loading}
+                cssOverride={override}
+                height={15}
+                width={3}
+                aria-label="Loading Spinner"
+                data-testid="loader"
+                />
+                
+            </div>
+            :<> 
+                <Header/>
+                <Home/>
+                <About/>
+                <Projects/>
+                <Contact/>
+                <Footer/>
+          </>
+        }
+      </div>
+       }/>
+        <Route path="*" element={
+          <NotFound />
+        } />
+       </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/NotFound.js
@@ -0,0 +1,51 @@
+import React from "react";
+import styled from "styled-components";
+// import image from "../static/5570862.jpg"
+
+const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  ${'' /* background:  lightblue url("${image}") no-repeat fixed center; */}
+  background:linear-gradient(75deg,#eef2f3,#8e9eab);
+  background-size:cover;
+  background-opacity:0.2;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Title = styled.h1`
+  font-size: 4rem;
+  margin: 0;
+`;
+
+const Button = styled.button`
+background-color:black;
+  color: #fff;
+  font-size: 1rem;
+  padding: 0.5rem 1rem;
+  margin:2rem 0;
+  border-radius: 0.5rem;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #555;
+  }
+`;
+
+const NotFoundPage = () => {
+  const handleGoHomeClick = () => {
+    window.location.href = "/";
+  };
+
+  return (
+    <Container>
+      <Title>404</Title>
+      <p>Oops! Page not found.</p>
+      <Button onClick={handleGoHomeClick}>Go Home</Button>
+    </Container>
+  );
+};
+
+export default NotFoundPage;
